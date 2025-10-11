22268569038000dd82d6c73d9019f786a239fd5a
PetrolPricesnearMe <petrolpricesnearme@gmail.com>
2025-10-11 12:49:42 +1100

    chore: Update frontend agent rules and configuration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import './index.css';
 import App from './App';
 
@@ -9,5 +10,6 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 ); 
